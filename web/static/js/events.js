2916942d59cb44aab6f8090930a887f9062edde1
valdiralves3000 <valdiralves3000@gmail.com>
2025-06-05 12:14:15 +0000

    feat(ui): add dynamic styling on file input change

--- web/static/js/events.js
+++ web/static/js/events.js
@@ -1,6 +1,6 @@
-import { VIEW } from './view.js';
-import { processFile } from './file_handler.js';
-import { handleTranscription } from './handler_transcription.js';
+import { VIEW } from './view.js?v=timestamp';
+import { processFile } from './file_handler.js?v=timestamp';
+import { handleTranscription } from './handler_transcription.js?v=timestamp';
 
 function preventDefaults(e) {
     e.preventDefault();
@@ -38,6 +38,13 @@ export function setupFileInput() {
     VIEW.fileInput.addEventListener('change', event => {
         if (event.target.files.length > 0) {
             processFile(event.target.files[0]);
+
+            VIEW.inputGroup.classList.add('active');
+            VIEW.uploadText.classList.add('hidden');
+            VIEW.fileInputLabel.classList.remove('bg-gradient-to-r', 'from-primary', 'to-secondary');
+            VIEW.fileInputLabel.classList.add('bg-transparent', 'text-secondary', 'underline', 'shadow-none', 'hover:text-primary');
+            VIEW.transcribeBtn.classList.remove('hidden');
+            VIEW.transcribeBtn.classList.add('flex');
         }
     });
 }
