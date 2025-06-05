5fd9068b6eb11035525aba203aca07acf3c7bf09
valdiralves3000 <valdiralves3000@gmail.com>
2025-06-05 12:21:10 +0000

    feat: add listenTranscript initialization to app startup

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,11 +1,13 @@
 import { setupDragAndDrop, setupFileInput, setupTranscribeButton } from './events.js';
 import { setupCopyButton } from './copy.js';
+import { listenTranscript } from './listen_transcript.js';
 
 function app() {
     setupDragAndDrop();
     setupFileInput();
     setupTranscribeButton();
-    setupCopyButton();
+    setupCopyButton();   
+    listenTranscript();
 }
 
 document.addEventListener('DOMContentLoaded', app);
